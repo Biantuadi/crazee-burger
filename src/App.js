bfdaff87f1d8859f91951f77951c5b1bb2de34fc
Kevin-beni <benibiantuadi@gmail.com>
2022-11-26 13:08:37 +0100

    🔥 F01: Delete test files

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [nom, setNom] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    alert(`Bonjour ${nom}`);
+    setNom("");
+  };
+
   return (
     <div className="App">
-      
+      <h1>Bienvenue chez nous !</h1>
+      <p>Connectez-vous</p>
+      <form action="submit" onSubmit={(e) => handleSubmit(e)}>
+        <input
+          type="name"
+          required
+          placeholder="Entrez votre prenom"
+          value={nom}
+          onChange={(e) => setNom(e.target.value)}
+        />
+        <input type="submit" value="Envoyer" />
+      </form>
     </div>
   );
 }
