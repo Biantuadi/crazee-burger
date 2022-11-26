0d412392f50c4516b01afcc3f572134cb082ba74
Kevin-beni <benibiantuadi@gmail.com>
2022-11-26 14:44:39 +0100

    📦 F02: Package.json > react-router-dom@6

---
+++ src/routes/Router.routes.jsx
@@ -0,0 +1,16 @@
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+// const Router = () => {
+//     return (
+//         <BrowserRouter>
+//             <Routes>
+//                 <Route path="/" element={<Home />} />
+//                 <Route path="/about" element={<About />} />
+//                 <Route path="/contact" element={<Contact />} />
+//             </Routes>
+            
+//         </BrowserRouter>
+//     );
+// };
+
+// export default Router;
