fd09faa0dfdcad740a4b89ffc02f9f544c4861d0
Kevin-beni <benibiantuadi@gmail.com>
2022-11-26 15:13:03 +0100

    ✨ F02: App systeme routing

---
+++ src/components/Pages/errors/ErrorPage.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function ErrorPage() {
+  return <h1>Erros</h1>;
+}
+
+export default ErrorPage;
--- src/routes/Router.routes.jsx
+++ src/routes/Router.routes.jsx
@@ -1,16 +1,18 @@
-// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LoginPage from "../components/Pages/auth/LoginPage";
+import DashboardPage from "../components/Pages/dashboard/DashboardPage";
+import ErrorPage from "../components/Pages/errors/ErrorPage";
 
-// const Router = () => {
-//     return (
-//         <BrowserRouter>
-//             <Routes>
-//                 <Route path="/" element={<Home />} />
-//                 <Route path="/about" element={<About />} />
-//                 <Route path="/contact" element={<Contact />} />
-//             </Routes>
-            
-//         </BrowserRouter>
-//     );
-// };
+const RouterRoutes = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/dashboard" element={<DashboardPage />} />
+        <Route path="*" element={<ErrorPage />} />
+      </Routes>
+    </Router>
+  );
+};
 
-// export default Router;
+export default RouterRoutes;
