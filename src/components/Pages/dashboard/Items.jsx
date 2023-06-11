aa038e23d98fc4ee3b8cf33e09a03ed6554d7be2
kevin <kevin@zonova.io>
2023-06-11 19:07:52 +0200

    ♻️ F05:Refactor menu

--- src/components/Pages/dashboard/Items.jsx
+++ src/components/Pages/dashboard/Items.jsx
@@ -1,101 +1,42 @@
 import React from "react";
 import styled from "styled-components";
-import { fakeMenu2 } from "../../../fakeData/fakeMenu";
-import { theme } from "../../../themes";
 
-export default function Items() {
-  const items = fakeMenu2;
-
+export default function Items({ title, imageSource, price, id }) {
   return (
-    <ItemsStyled>
-      {items.map((item) => (
-        <li key={item.id}>
-          <img src={item.imageSource} alt={item.title} />
-          <h3>{item.title}</h3>
-          <div className="price_button">
-            <span>{item.price}</span>
-            <button>Buy</button>
-          </div>
-        </li>
-      ))}
-    </ItemsStyled>
+    <>
+      <ItemStyled key={id}>
+        <div className="container_img">
+          <img src={imageSource} alt={title} />
+        </div>
+        <h3>{title}</h3>
+        <div className="price_button">
+          <span>{price}</span>
+          <button>Buy</button>
+        </div>
+      </ItemStyled>
+    </>
   );
 }
 
-const ItemsStyled = styled.ul`
-    display: grid;
-    grid-template-columns: repeat(4, 1fr);
-    grid-gap: 10px;
-    justify-items: center;
-    padding: 10px;
-    margin: 10px;
-    margin-top: 0;
-    height: 80vh;
-    overflow: auto;
-    overflow-x: hidden;
+const ItemStyled = styled.li`
+  width: 240px;
+  height: 330px;
+  background-color: #fff;
+  margin: 10px;
+  padding: 10px;
+  border-radius: 10px;
+  box-shadow: 1px 1px 10px 0 rgba(0, 0, 0, 0.1);
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
 
-    /* scroll bar */
+  img {
+    width: 130px;
+    height: 120px;
+  }
 
-    ::-webkit-scrollbar {
-      width: 15px;
-    }
-    
-    
-    
-    &::-webkit-scrollbar-track {
-      background: transparent;
-    }
-    
-    &::-webkit-scrollbar-thumb {
-      border-radius: 10px;
-      background: ${theme.colors.greyDark};
-    }
-
-    &::-webkit-scrollbar-thumb:hover {
-      background: #555;
-    }
-
-
-
-
-    @media (max-width: 1294px) and (min-width: 891px) {
-      grid-template-columns: repeat(3, 1fr);
-    }
-
-    @media (max-width: 1091px) and (min-width: 630px) {
-      grid-template-columns: repeat(2, 1fr);
-      height: 87vh;
-    }
-
-    @media (max-width: 768px) {
-      height: 100%;
-    }
-
-    @media (max-width: 630px) and (min-width: 0px) {
-      grid-template-columns: repeat(1, 1fr);
-      height: 100%;
-    }
-
-    li {
-      width: 195px;
-      height: 290px;
-      background-color: #fff;
-      margin: 10px;
-      padding: 10px;
-      border-radius: 10px;
-      box-shadow: 1px 1px 10px 0 rgba(0, 0, 0, 0.1);
-      display: flex;
-      flex-direction: column;
-      justify-content: space-around;
-      align-items: center;
-
-      img {
-        width: 130px;
-        height: 120px;
-      }
-
-      h3 {
-        font-size: 30px;
-      }
-    }
+  h3 {
+    font-size: 30px;
+  }
 `;
--- src/components/Pages/dashboard/SectionMenu.jsx
+++ src/components/Pages/dashboard/SectionMenu.jsx
@@ -1,13 +1,73 @@
 import styled from "styled-components";
 import Items from "./Items";
-
+import { fakeMenu2 } from "../../../fakeData/fakeMenu";
+import { useState } from "react";
+import { theme } from "../../../themes";
 
 export default function SectionMenu() {
+  const [products, setProducts] = useState(fakeMenu2);
   return (
     <SectionMenuStyled>
-      <Items />
+      <ul>
+        {products.map((item) => (
+          <Items
+            title={item.title}
+            imageSource={item.imageSource}
+            price={item.price}
+            id={item.id}
+          />
+        ))}
+      </ul>
     </SectionMenuStyled>
   );
 }
 
-const SectionMenuStyled = styled.section``;
+const SectionMenuStyled = styled.section`
+  ul {
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    grid-gap: 60px;
+    justify-items: center;
+    padding: 10px;
+    margin: 10px;
+    margin-top: 0;
+    height: 80vh;
+    overflow: scroll;
+    overflow-x: hidden;
+
+    ::-webkit-scrollbar {
+      width: 15px;
+    }
+
+    &::-webkit-scrollbar-track {
+      background: transparent;
+    }
+
+    &::-webkit-scrollbar-thumb {
+      border-radius: 10px;
+      background: ${theme.colors.greyDark};
+    }
+
+    &::-webkit-scrollbar-thumb:hover {
+      background: #555;
+    }
+
+    @media (max-width: 1294px) and (min-width: 891px) {
+      grid-template-columns: repeat(3, 1fr);
+    }
+
+    @media (max-width: 1091px) and (min-width: 630px) {
+      grid-template-columns: repeat(2, 1fr);
+      height: 87vh;
+    }
+
+    @media (max-width: 768px) {
+      height: 100%;
+    }
+
+    @media (max-width: 630px) and (min-width: 0px) {
+      grid-template-columns: repeat(1, 1fr);
+      height: 100%;
+    }
+  }
+`;
